f9f554eae7e1e8d9c00e6cb98edefbd4449bcb2c
asmaa-kashef <asmaa.ali.kashef@gmail.com>
2025-09-12 09:35:52 +0400

    Update website

--- next.config.ts
+++ next.config.ts
@@ -10,12 +10,32 @@ const nextConfig: NextConfig = {
                 pathname: "/**",
             },
             {
-                protocol: 'https',
-                hostname: 'lh3.googleusercontent.com',
-                port: '',
-                pathname: '/**',
+                protocol: "https",
+                hostname: "lh3.googleusercontent.com",
+                port: "",
+                pathname: "/**",
             },
         ],
+    },
+
+    async redirects() {
+        return [
+            {
+                source: "/VillaConstruction/-contracting-companies-in-abu-dhabi-top-firms-and-industry-insights",
+                destination: "/", // الصفحة الرئيسية
+                permanent: true,
+            },
+            {
+                source: "/VillaConstruction",
+                destination: "/", // الصفحة الرئيسية
+                permanent: true,
+            },
+            {
+                source: "/building-maintenance-companies-in-abu-dhabi",
+                destination: "/", // الصفحة الرئيسية
+                permanent: true,
+            },
+        ];
     },
 };
 
