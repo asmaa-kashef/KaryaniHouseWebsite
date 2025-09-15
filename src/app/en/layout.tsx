69eb45c5c6b314d42d7c1d005384cda3bed63c84
asmaa-kashef <asmaa.ali.kashef@gmail.com>
2025-09-15 12:37:48 +0400

    Force redeploy4

--- src/app/en/layout.tsx
+++ src/app/en/layout.tsx
@@ -1,34 +1,29 @@
 ﻿// app/layout.tsx
-
 import Script from "next/script";
-
-/// Corrected and recommended code
 import "../../../public/css/bootstrap.css";
 import "../../../public/css/style.css";
 import "../../../public/css/responsive.css";
 
-
-
-export default function RootLayout({
-    children,
-}: {
-    children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="en">
             <body>
                 <main>{children}</main>
-                <Script src="/js/jquery.js" strategy="beforeInteractive" />
-                <Script src="/js/popper.min.js" strategy="beforeInteractive" />
-                <Script src="/js/bootstrap.min.js" strategy="lazyOnload" />
-                <Script src="/js/jquery.fancybox.js" strategy="lazyOnload" />
-                <Script src="/js/owl.js" strategy="lazyOnload" />
-                <Script src="/js/jquery.mCustomScrollbar.concat.min.js" strategy="lazyOnload" />
-                <Script src="/js/wow.js" strategy="lazyOnload" />
-                <Script src="/js/appear.js" strategy="lazyOnload" />
-                <Script src="/js/mixitup.js" strategy="lazyOnload" />
-                <Script src="/js/script.js" strategy="lazyOnload" />
-                <Script src="/js/color-settings.js" strategy="lazyOnload" />
+
+                {/* Load jQuery and Popper first, but safely */}
+                <Script src="/js/jquery.js" strategy="afterInteractive" />
+                <Script src="/js/popper.min.js" strategy="afterInteractive" />
+
+                {/* Other legacy scripts */}
+                <Script src="/js/bootstrap.min.js" strategy="afterInteractive" />
+                <Script src="/js/jquery.fancybox.js" strategy="afterInteractive" />
+                <Script src="/js/owl.js" strategy="afterInteractive" />
+                <Script src="/js/jquery.mCustomScrollbar.concat.min.js" strategy="afterInteractive" />
+                <Script src="/js/wow.js" strategy="afterInteractive" />
+                <Script src="/js/appear.js" strategy="afterInteractive" />
+                <Script src="/js/mixitup.js" strategy="afterInteractive" />
+                <Script src="/js/script.js" strategy="afterInteractive" />
+                <Script src="/js/color-settings.js" strategy="afterInteractive" />
             </body>
         </html>
     );
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
     children: React.ReactNode;
 }) {
     return (
-        <html>
+        <html lang="en">
             <body>
                 <main>{children}</main>
                 <Script src="/js/jquery.js" strategy="beforeInteractive" />
