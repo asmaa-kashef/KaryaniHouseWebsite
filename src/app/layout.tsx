9ca55e8d3817b4cd02b6743e070fd910d1389fa1
asmaa-kashef <asmaa.ali.kashef@gmail.com>
2025-09-15 12:19:31 +0400

    Force redeploy3

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
     children: React.ReactNode;
 }) {
     return (
-        <html lang="en">
+        <html>
             <body>
                 <main>{children}</main>
                 <Script src="/js/jquery.js" strategy="beforeInteractive" />
