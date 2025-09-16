2a5fa3df0d767f93954d150759e3b2a19f1516cb
asmaa-kashef <asmaa.ali.kashef@gmail.com>
2025-09-16 10:50:09 +0400

    Force redeploy9

--- src/app/en/layout.tsx
+++ src/app/en/layout.tsx
@@ -1,35 +1,55 @@
 ﻿// app/layout.tsx
-
 import Script from "next/script";
 
-/// Corrected and recommended code
 import "../../../public/css/bootstrap.css";
 import "../../../public/css/style.css";
 import "../../../public/css/responsive.css";
 
-
-
-export default function RootLayout({
-    children,
-}: {
-    children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
-        <html lang="en">
-            <body>
-                <main>{children}</main>
-                <Script src="/js/jquery.js" strategy="beforeInteractive" />
-                <Script src="/js/popper.min.js" strategy="beforeInteractive" />
-                <Script src="/js/bootstrap.min.js" strategy="lazyOnload" />
-                <Script src="/js/jquery.fancybox.js" strategy="lazyOnload" />
-                <Script src="/js/owl.js" strategy="lazyOnload" />
-                <Script src="/js/jquery.mCustomScrollbar.concat.min.js" strategy="lazyOnload" />
-                <Script src="/js/wow.js" strategy="lazyOnload" />
-                <Script src="/js/appear.js" strategy="lazyOnload" />
-                <Script src="/js/mixitup.js" strategy="lazyOnload" />
-                <Script src="/js/script.js" strategy="lazyOnload" />
-                <Script src="/js/color-settings.js" strategy="lazyOnload" />
-            </body>
-        </html>
+        <>
+            <main>{children}</main>
+
+            {/* جافاسكريبت الموقع */}
+            <Script src="/js/jquery.js" strategy="beforeInteractive" />
+            <Script src="/js/popper.min.js" strategy="beforeInteractive" />
+            <Script src="/js/bootstrap.min.js" strategy="lazyOnload" />
+            <Script src="/js/jquery.fancybox.js" strategy="lazyOnload" />
+            <Script src="/js/owl.js" strategy="lazyOnload" />
+            <Script src="/js/jquery.mCustomScrollbar.concat.min.js" strategy="lazyOnload" />
+            <Script src="/js/wow.js" strategy="lazyOnload" />
+            <Script src="/js/appear.js" strategy="lazyOnload" />
+            <Script src="/js/mixitup.js" strategy="lazyOnload" />
+            <Script src="/js/script.js" strategy="lazyOnload" />
+            <Script src="/js/color-settings.js" strategy="lazyOnload" />
+
+            {/* Meta Pixel */}
+            <Script id="meta-pixel" strategy="afterInteractive">
+                {`
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '1254073446082094');
+          fbq('track', 'PageView');
+          fbq('track', 'Lead');
+          fbq('track', 'AddToCart');
+          fbq('track', 'Purchase', {value: 0.00, currency: 'USD'});
+        `}
+            </Script>
+
+            <noscript>
+                <img
+                    height="1"
+                    width="1"
+                    style={{ display: 'none' }}
+                    src="https://www.facebook.com/tr?id=1254073446082094&ev=PageView&noscript=1"
+                />
+            </noscript>
+        </>
     );
 }
