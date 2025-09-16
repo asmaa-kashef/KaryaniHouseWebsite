f4ce7558610a609d1718939f8489a3996acb289b
asmaa-kashef <asmaa.ali.kashef@gmail.com>
2025-09-16 14:51:57 +0400

    force depolay 15

--- src/app/en/layout.tsx
+++ src/app/en/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             t.src=v;s=b.getElementsByTagName(e)[0];
             s.parentNode.insertBefore(t,s)}(window, document,'script',
             'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', 'YOUR_PIXEL_ID');
+            fbq('init', '1254073446082094');
             fbq('track', 'PageView');
           `}
                 </Script>
