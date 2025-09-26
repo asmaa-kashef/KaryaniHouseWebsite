5c77d6337cc7b94b6c7c35ba8d343e0e7cd723a0
asmaa-kashef <asmaa.ali.kashef@gmail.com>
2025-09-26 15:03:21 +0400

    Website Update

--- src/app/components/ProjectsInsightChart.tsx
+++ src/app/components/ProjectsInsightChart.tsx
@@ -1,7 +1,7 @@
 ﻿"use client";
 
 import React, { useEffect, useState } from "react";
-import Image from "next/image";
+
 import { usePathname } from "next/navigation";
 
 // ---------- Types ----------
