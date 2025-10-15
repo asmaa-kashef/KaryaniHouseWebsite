491f244a5265287c400c1035323729f085497b1f
asmaa-kashef <asmaa.ali.kashef@gmail.com>
2025-10-15 14:35:15 +0400

    Updated VillaConstruction page6

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export const metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
-        <html lang="ar">
+        <html lang="en">
             <head>
                 {/* ---------------------------------------------------- */}
                 {/* 1. GOOGLE TAG MANAGER (GTM) - الجزء الأول في الـ <HEAD> */}
